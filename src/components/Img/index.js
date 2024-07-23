7935f0b9e699033256249a089b0c7e0c61392095
Sabienss <121949844+Sabienss@users.noreply.github.com>
2024-07-23 13:32:46 +0500

    deletedthenaddnew

--- src/components/Img/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const Img = ({ className, src = "defaultNoData.png", alt = "testImg", ...restprops }) => {
-return <img className={className} src={src} alt={alt} {...restProps} loading={"lazy"} />;
-};
-export { Img };
